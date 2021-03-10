86c9405c2792a9cf30c29f3b90e50b30f0c123a6
Javier Botero <javierbotero10271@gmail.com>
2021-03-10 00:04:34 -0500

    Adds the basic components

---
+++ src/components/Course.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import ReactRouterPropTypes from 'react-router-prop-types';
+import PropTypes from 'prop-types';
+
+const Course = props => {
+  const { match, courses } = props;
+  const course = courses.find(c => c.id === match.props.id);
+
+  return (
+    <div>
+      <div>
+        <div>{course.title}</div>
+        <div>
+          Teacher:
+          {` ${course.teacher.username}`}
+        </div>
+      </div>
+      <div>{course.content}</div>
+      <div>
+        status:
+        {` ${course.status}`}
+      </div>
+    </div>
+  );
+};
+
+Course.propTypes = {
+  match: ReactRouterPropTypes.match.isRequired,
+  courses: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+    teacher_id: PropTypes.number.isRequired,
+    status: PropTypes.string.isRequired,
+    dates: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    created_at: PropTypes.string.isRequired,
+    updated_at: PropTypes.string.isRequired,
+    teacher: PropTypes.shape({
+      id: PropTypes.number,
+      username: PropTypes.string,
+    }).isRequired,
+    favorites: PropTypes.arrayOf(PropTypes.shape({
+      course_id: PropTypes.number,
+      user_id: PropTypes.number,
+    })).isRequired,
+    subscriptions: PropTypes.arrayOf(PropTypes.shape({
+      course_id: PropTypes.number,
+      user_id: PropTypes.number.isRequired,
+      confirmed: PropTypes.bool,
+    })).isRequired,
+    comments: PropTypes.arrayOf(PropTypes.shape({
+      user_id: PropTypes.number,
+      course_id: PropTypes.number,
+      body: PropTypes.string,
+    })).isRequired,
+    pendings: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.number,
+      user_id: PropTypes.number,
+      course_id: PropTypes.number,
+      confirmed: PropTypes.bool,
+    })).isRequired,
+  })).isRequired,
+};
+
+export default Course;
---
+++ src/containers/App.js
@@ -0,0 +1,90 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import Course from '../components/Course';
+
+const App = props => {
+  const {
+    getCourses,
+    objThunk,
+    url,
+    token,
+    id,
+    tokenPayload,
+    // userPayload,
+  } = props;
+  const dispatch = useDispatch();
+  const courses = useSelector(state => state.courses.courses);
+  const stateCourses = useSelector(state => state.courses.status);
+  const tokenData = tokenPayload(id, token);
+  // const userData = userPayload();
+  const dataThunkCourses = objThunk(
+    url,
+    'POST',
+    {
+      ...tokenData,
+    },
+  );
+  useEffect(() => {
+    if (stateCourses === 'idle') {
+      dispatch(getCourses(dataThunkCourses));
+    }
+  }, [stateCourses, getCourses, dispatch, dataThunkCourses]);
+  const coursesToDivs = courses => courses.map(course => (
+    <div key={course.id}>
+      <div>Some picture</div>
+      <div>{course.title}</div>
+      <div>
+        <a href={`/${course.id}`}>More Info</a>
+      </div>
+    </div>
+  ));
+  return (
+    <div>
+      <BrowserRouter>
+        <header>
+          <h1>Find a fun course!</h1>
+        </header>
+        <Switch>
+          <Route exact path="/">
+            {coursesToDivs(courses)}
+          </Route>
+          <Route
+            exact
+            path="/:id"
+            render={({ match }) => (
+              <div>
+                <nav>
+                  <ul>
+                    <li>
+                      <Link to="/">&#60;</Link>
+                    </li>
+                  </ul>
+                </nav>
+                <Course match={match} courses={courses} />
+              </div>
+            )}
+          />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+App.propTypes = {
+  getCourses: PropTypes.func.isRequired,
+  objThunk: PropTypes.func.isRequired,
+  url: PropTypes.string.isRequired,
+  token: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  tokenPayload: PropTypes.func.isRequired,
+  // userPayload: PropTypes.func.isRequired,
+};
+
+export default App;
